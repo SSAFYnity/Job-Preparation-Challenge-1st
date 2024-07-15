8d349d3be164679e536f514ec7c438cbc10a862c
조은영 <hdtv0218@gmail.com>
2024-07-15 22:51:33 +0900

    #6 solved 붕대감기

---
+++ Programmers/붕대감기/붕대감기_조은영.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> bandage, int health, vector<vector<int>> attacks) {
+    int time = 0;  // 현재 시간
+    int answer = health;  // 결과값
+    int continueCnt = 0;  // 연속 기술 횟수
+    int attackCnt = 0;  // 공격 횟수
+
+    int skillTime = bandage[0];
+    int healthUp = bandage[1];
+    int bonus = bandage[2];
+
+    while (1) {
+
+        time++;
+
+        // 공격
+            if (attacks[attackCnt][0] == time && attackCnt < attacks.size()) {
+                answer -= attacks[attackCnt][1];
+                continueCnt = 0;
+                attackCnt++;
+            }
+        // 공격X
+            else {
+                answer += healthUp;
+
+                continueCnt++;
+
+                if (continueCnt == skillTime) {
+                    continueCnt = 0;
+                    answer += bonus;
+                }
+
+            }
+        if (answer > health) answer = health;
+        if (answer <= 0) return -1;
+        if (attackCnt = attacks.size()) break;
+    }
+
+
+
+    return answer;
+}
