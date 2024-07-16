36ee76bac44b43ded7174721056ece5af05c9d99
olrlobt <317tmdgjs@naver.com>
2024-07-16 20:18:05 +0900

    #7 reviewed 암호생성기

--- SWEA/암호생성기/암호생성기_이승헌.java
+++ SWEA/암호생성기/암호생성기_이승헌.java
@@ -21,22 +21,25 @@ public class 암호생성기_이승헌 {
                 queue.offer(Integer.parseInt(st.nextToken()));
             }
 
-            int poll;
-            int i = 1;
+            int curNum;
+            int i = 0;
             do {
-                poll = queue.poll();
-                poll -= i++;
-                i = i > 5 ? 1 : i;
+                curNum = queue.poll();
+//                poll -= i++;
+//                i = i > 5 ? 1 : i;
+                i %= 5; // 기존 조건식에서 모듈러 연산으로 변경
+                curNum -= ++i;
 
-                queue.offer(poll);
-            } while (poll > 0);
+                queue.offer(curNum);
+            } while (curNum > 0);
 
             sb.append("#").append(testCase).append(" ");
             for (int idx = 0; idx < 7; idx++) {
                 sb.append(queue.poll()).append(" ");
             }
+            queue.poll(); // clear() 대신 poll로 비워주고, 0을 추가하여 마무리
             sb.append(0).append("\n");
-            queue.clear();
+//            queue.clear();
         }
         System.out.println(sb.toString());
     }
