3d3883fcef1bc66bd8286f77414edb8bbe1722fb
DoHyun <yup8229@gmail.com>
2024-07-23 23:26:01 +0900

    solved N번째큰수 - 824ms 274172kb

---
+++ Baekjoon/N번째큰수/N번째큰수_권도현.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int x = Integer.parseInt(st.nextToken());
+                q.add(x);
+            }
+        }
+        for (int i = 0; i < N - 1; i++) {
+            q.poll();
+        }
+        System.out.println(q.poll());
+    }
+}
