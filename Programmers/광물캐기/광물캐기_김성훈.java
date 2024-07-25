770bba0306d4b0f3136f5277155c1e20b8324809
성훈 <ksh7412.dev@gmail.com>
2024-07-26 00:02:37 +0900

    #196 solved 광물캐기 - 0.68ms 64.3mb
    12시는 넘겼지만 풀었습니다

--- Programmers/광물캐기/광물캐기_김성훈.java
+++ Programmers/광물캐기/광물캐기_김성훈.java
@@ -9,22 +9,23 @@ class Solution {
         mineralMap.put("iron", 1000);
         mineralMap.put("stone", 10);
 
-        int size = minerals.length;
-        int[] mineralToInt = new int[size];
-        for (int i = 0; i < size; i++) {
+        int mineralSizes = minerals.length;
+        int[] mineralToInt = new int[mineralSizes];
+        for (int i = 0; i < mineralSizes; i++) {
             mineralToInt[i] = mineralMap.get(minerals[i]);
         }
 
-        int[] tired = new int[(size + 4) / 5];
+        int pickSizes = picks[0] + picks[1] + picks[2];
+        int[] tired = new int[pickSizes];
 
-        for (int i = 0; i < tired.length; i++) {
+        for (int i = 0; i < pickSizes; i++) {
             for (int j = 0; j < 5; j++) {
-                if (i * 5 + j >= size) break;
+                if (i * 5 + j >= mineralSizes) break;
                 tired[i] -= mineralToInt[i * 5 + j];
             }
         }
-        int[] tiredSort = tired.clone();
-        Arrays.sort(tiredSort);
+
+        Arrays.sort(tired);
 
         int answer = 0;
         for (int i = 0; i < tired.length; i++) {
@@ -36,9 +37,9 @@ class Solution {
             } else if (picks[1] != 0) {
                 picks[1]--;
                 answer -= tired[i] / 100000 * 5;
-                answer -= (tired[i] % 100000) / 1000;
+                answer -= tired[i] % 100000 / 1000;
                 answer -= tired[i] % 1000 / 10;
-            } else if (picks[2] > 0){
+            } else if (picks[2] != 0){
                 picks[2]--;
                 answer -= tired[i] / 100000 * 25;
                 answer -= tired[i] % 100000 / 1000 * 5;
