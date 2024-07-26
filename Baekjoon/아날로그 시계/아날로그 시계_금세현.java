a0f6be4a231767f72cb81e3fa3c3b0b7f2053551
shkum0330 <shkum0330@gmail.com>
2024-07-26 10:42:44 +0900

    solved 아날로그시계

--- Baekjoon/아날로그 시계/아날로그 시계_금세현.java
+++ Baekjoon/아날로그 시계/아날로그 시계_금세현.java
@@ -1,32 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+class Solution {
+    public int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
+        int answer = -1;
+        int start=h1*3600+m1*60+s1;
+        int end=h2*3600+m2*60+s2;
 
-public class Main {
-    static StringBuilder sb=new StringBuilder();
-    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int n;
-    static int[][] map =new int[1501][1501];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        answer = end*59/3600 - start*59/3600 + end*719/43200-start*719/43200;
 
-        n=Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0;i<n;i++){
-            st=new StringTokenizer(br.readLine());
-            for(int j=0;j<n;j++){
-                pq.add(Integer.parseInt(st.nextToken()));
-            }
+        if(start>=43200){
+            answer += 1;
         }
 
-        for (int i = 0; i < n-1; i++) {
-            pq.poll();
+        if(end>=43200){
+            answer -= 1;
         }
-
-        System.out.println(pq.peek());
+        if(start * 59 % 3600 == 0 || start * 719 % 43200 == 0){
+            answer += 1;
+        }
+        return answer;
     }
 }
