b92b58c7ae0a92efd8d2507b49be83c78324b203
DHniyeo <ehdgns915@naver.com>
2024-08-16 19:50:00 +0900

    solved 팰린드롬 - 16 2020

--- mybash.sh
+++ mybash.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # 문제 번호 입력 받기
-read -p "문제 번호를 입력하세요: " remote_name
+name_val = "여동훈"
 
+read -p "문제명을 입력하세요: " problem_name
+read -p "걸린 시간을 입력하세요: " time
+read -p "사용한 메모리를 입력하세요: " memory
 
 # 로컬 브랜치 이름 생성
-branch_name="${problem_number}"
+branch_name= $name_val'_'$problem_name
 
 # Git 작업 수행
-git fetch origin main:feature
-git pull origin main
+# git fetch algorithm main:master
+# git pull algorithm main
 git branch $branch_name
 git checkout $branch_name
-git add -A
-git commit -m "solved LeetCode $problem_number. Valid Palindrome - 16ms 2060kb"
+git add .
+git commit -m "solved $problem_name - $time $memory"
 git push algorithm $branch_name
