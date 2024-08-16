238dfaeeeb0b5ca7b9e2188c6f63b05fa66474f3
DHniyeo <ehdgns915@naver.com>
2024-08-17 03:01:48 +0900

    solved 겹치는건싫어 - 72 ms 3588 kb

---
+++ Baekjoon/겹치는건싫어/여동훈_겹치는건싫어.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int mem[100001];
+int num;
+int max_cnt;
+int result;
+queue<int> q;
+int arr[200000];
+
+void init() {
+	cin >> num;
+	cin >> max_cnt;
+	for (int i = 0; i < num; i++) {
+		cin >> arr[i];
+	}
+}
+void solve() {
+	for (int i = 0; i < num; i++) {
+		mem[arr[i]]++;
+		if (mem[arr[i]] > max_cnt) { // 해당 카운트를 넘으면
+			while (mem[arr[i]] > max_cnt) { // 
+				int front = q.front(); q.pop();
+				mem[front]--;
+			}
+		}
+		q.push(arr[i]); // push
+		result = result < q.size() ? q.size() : result;
+
+	}
+
+
+}
+void result_print() {
+	cout << result;
+}
+int main() {
+	init();
+	solve();
+	result_print();
+}
