9daaebcc45dfc828b6a7167af092660c1f3bf6ae
강수민 <kangsm423@gmail.com>
2024-08-21 22:25:00 +0900

    solved 퇴사 - 212ms 17.832mb

---
+++ Baekjoon/퇴사/퇴사_강수민.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] t = new int[n];
+        int[] p = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            t[i] = sc.nextInt();
+            p[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            if (i + t[i] <= n) {
+                dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
+            }
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        }
+        System.out.println(dp[n]);
+    }
+}
