9812a4b7001e36fb0e2d14aecdb0979323cce18d
조은영 <hdtv0218@gmail.com>
2024-08-26 14:11:38 +0900

    solved 돌게임 - 0ms 2mb

---
+++ Baekjoon/돌게임/돌게임_조은영.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	if (n % 2 == 0) {
+		cout << "CY" << "\n";
+	}
+	else {
+		cout << "SK" << "\n";
+	}
+
+	return 0;
+}
