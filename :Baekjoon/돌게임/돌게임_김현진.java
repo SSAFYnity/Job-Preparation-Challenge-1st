d0f42ebf159bab96fe57834b614e7c831745d735
hyunjin <tcmhdn77@naver.com>
2024-08-26 23:51:42 +0900

    solved 돌 게임 - 124ms 14MB

---
+++ :Baekjoon/돌게임/돌게임_김현진.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 돌게임_김현진 {
+	// 9655 돌 게임
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(sc.readLine());
+
+		if (N % 2 == 0)
+			System.out.println("CY");
+		else
+			System.out.println("SK");
+	}
+
+}
