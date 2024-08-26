2bf6820b9bf2f0f99c65413e8073b6c6d2304095
souk0712 <souk0712@gmail.com>
2024-08-26 22:09:10 +0900

    solved 돌게임 - 64ms 11.516mb

---
+++ Baekjoon/돌게임/돌게임_이아름.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N % 2 == 0 ? "CY" : "SK");
+    }
+}
