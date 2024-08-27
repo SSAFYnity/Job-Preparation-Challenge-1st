57ca1d5aaa403845afdaf896f647b8335402cdfc
강수민 <kangsm423@gmail.com>
2024-08-27 23:37:58 +0900

    sovled 돌게임 - 100ms 14.292mb

---
+++ Baekjoon/돌게임/돌게임_강수민.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(Integer.parseInt(br.readLine()) % 2 == 0 ? "CY" : "SK");
+        bw.flush();
+    }
+}
