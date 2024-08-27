00fd9242a834ee6e0e1f742d37f9c8ea6451c938
JongHyeon Kim (Bell) <woaol@naver.com>
2024-08-27 13:12:07 +0000

    solved 돌게임 - 12.900mb 112ms

---
+++ Baekjoon/돌게임/돌게임_김종현.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Scanner(System.in).nextInt() % 2 == 0 ? "CY" : "SK");
+    }
+}
